01af9778a8e266c4dfbb762cc770a43d6fdae23e
Syahrizal Hanif <syahrizalhanif@gmail.com>
2023-04-30 16:59:34 +0700

    Update favicon

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -10,12 +10,13 @@
 
     <title><?= $data['title']; ?></title>
 
+    <link rel="icon" type="image/x-icon" href="<?= BASE_URL . 'public/img/favicon.ico'; ?>">
+
     <!-- Custom fonts for this template-->
     <link href="<?= BASE_URL . 'public/vendor/fontawesome-free/css/all.min.css'; ?>" rel="stylesheet" type="text/css">
     <link
             href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
             rel="stylesheet">
-
     <!-- Custom styles for this template-->
     <link href="<?= BASE_URL . 'public/css/sb-admin-2.min.css'; ?>" rel="stylesheet">
 </head>
